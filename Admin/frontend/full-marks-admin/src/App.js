79b148877573a43314fadd0b5a94c25161499c70
ARPIT <arpitdude006@gmail.com>
2024-05-29 16:13:58 +0530

    5

--- Admin/frontend/full-marks-admin/src/App.js
+++ Admin/frontend/full-marks-admin/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSchool from './Users/School/AddSchool';
+import AddTeacher from './Users/Teachers/AddTeacher';
+import AddStudent from './Users/Students/AddStudent';
 
 function App() {
   return (
     <div className="App">
       
-      <AddSchool/>
+      <AddStudent/>
     </div>
   );
 }
---
+++ Admin/frontend/full-marks-admin/src/Users/Students/AddStudent.js
@@ -0,0 +1,218 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import {
+    CitySelect,
+    CountrySelect,
+    StateSelect,
+} from "react-country-state-city";
+
+import "react-country-state-city/dist/react-country-state-city.css";
+
+const AddStudent = () => {
+    const [formData, setFormData] = useState({
+        company_name: '',
+        studentname: '',
+        password: '',
+        profilepic: null,
+        email: '',
+        contact_no: '',
+        country: '',
+        state: '',
+        city: '',
+        zipcode: ''
+    });
+
+    const [countryId, setCountryId] = useState(null);
+    const [stateId, setStateId] = useState(null);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleFileChange = (e) => {
+        setFormData({
+            ...formData,
+            logo: e.target.files[0]
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+        Object.keys(formData).forEach(key => {
+            data.append(key, formData[key]);
+        });
+
+        axios.post('http://localhost/fullmarks-server/addschools.php', data)
+            .then(response => {
+                if (response.data.success) {
+                    alert('School added successfully');
+                    setFormData({
+                        company_name: '',
+                        studentname: '',
+                        password: '',
+                        profilepic: null,
+                        email: '',
+                        contact_no: '',
+                        country: '',
+                        state: '',
+                        city: '',
+                        zipcode: ''
+                    });
+                } else {
+                    alert('Error adding school');
+                }
+            })
+            .catch(error => console.error('Error submitting form:', error));
+    };
+    return (
+        <div> <Form onSubmit={handleSubmit}>
+
+            <Form.Group className="mb-3" controlId="formSchoolAddress">
+            <Form.Label>Class</Form.Label>
+            <Form.Select aria-label="Default select example">
+                <option>Select-Class</option>
+                <option value="1st Class">1st Class</option>
+                <option value="2st Class">2nd Class</option>
+                <option value="3rd Class">3rd Class</option>
+                <option value="4th Class">4th Class</option>
+                <option value="5th Class">4th Class</option>
+                <option value="6th Class">6th Class</option>
+                <option value="7th Class">7th Class</option>
+                <option value="8th Class">8th Class</option>
+                <option value="9th Class">9th Class</option>
+                <option value="10th Class">10th Class</option>
+                <option value="11th Class">11th Class</option>
+                <option value="12th Class">12th Class</option>
+                <option value="Pre-Primary">Pre-Primary</option>
+                <option value="Praveshika/0 part">Praveshika/0 part</option>
+                <option value="Beginner">Beginner</option>
+                <option value="NA">NA</option>
+                <option value="9-10th">9-10th</option>
+                <option value="11-12th">11th-12th</option>
+                <option value="Primary">Primary</option>
+            </Form.Select>
+            </Form.Group>
+
+
+            <Form.Group className="mb-3" controlId="formSchoolAddress">
+                <Form.Label>Student Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Student Name"
+                    name="studentname"
+                    value={formData.studentname}
+                    onChange={handleChange}
+                    required
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formContactNo">
+                <Form.Label>Contact Number</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Contact Number"
+                    name="contact_no"
+                    value={formData.contact_no}
+                    onChange={handleChange}
+                    required
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formFile" className="mb-3">
+                <Form.Label>Profile Image</Form.Label>
+                <Form.Control
+                    type="file"
+                    name="profilepic"
+                    onChange={handleFileChange}
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formCountry">
+                <Form.Label>Select Country</Form.Label>
+                <CountrySelect
+                    onChange={(country) => {
+                        setCountryId(country.id);
+                        setFormData({ ...formData, country: country.name });
+                    }}
+                    placeHolder="Select Country"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formState">
+                <Form.Label>Select State</Form.Label>
+                <StateSelect
+                    countryid={countryId}
+                    onChange={(state) => {
+                        setStateId(state.id);
+                        setFormData({ ...formData, state: state.name });
+                    }}
+                    placeHolder="Select State"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formCity">
+                <Form.Label>Select City</Form.Label>
+                <CitySelect
+                    countryid={countryId}
+                    stateid={stateId}
+                    onChange={(city) => {
+                        setFormData({ ...formData, city: city.name });
+                    }}
+                    placeHolder="Select City"
+                />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formContactNo">
+                <Form.Label>Zip Code</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Zip Code"
+                    name="zipcode"
+                    value={formData.zipcode}
+                    onChange={handleChange}
+                    required
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+
+        </Form>
+        </div>
+    )
+}
+
+export default AddStudent
